576f249e0a12cdc8950b364c0227ad39d4896ea9
Sagar Bansal <sagarbansal099@gmail.com>
2018-09-20 16:37:11 +0000

    modify register

--- js/custom.js
+++ js/custom.js
@@ -17,3 +17,44 @@ $(remove_button).click(function(e){
 		wrapper[0].childNodes[total_fields-1].remove();
 	}
 });
+
+
+
+var members = $("#members");
+var permanent = $(".permanent");
+var lock = $("#lock:checked");
+var temp = $(".temp");
+const my = '<div class="temp row g-margin-b-50--xs">'
+                            + '<div class="col-sm-6 g-margin-b-30--xs g-margin-b-0--md">'
+                                +'<input type="text" class="form-control s-form-v3__input" placeholder="* Member 1">'
+                            +'</div>'
+                            +'<div class="col-sm-6">'
+                              +'  <input type="text" class="form-control s-form-v3__input" placeholder="* Phone 1">'
+                            +'</div>'
+                        +'</div>';
+
+function stringvalue(i){
+	return '<div class="temp row g-margin-b-50--xs">'
+                            + '<div class="col-sm-6 g-margin-b-30--xs g-margin-b-0--md">'
+                                +'<input type="text" class="form-control s-form-v3__input" placeholder="* Member '+i+'">'
+                            +'</div>'
+                            +'<div class="col-sm-6">'
+                              +'  <input type="text" class="form-control s-form-v3__input" placeholder="* Phone '+i+'">'
+                            +'</div>'
+                        +'</div>';
+}
+
+// alert(members.val());
+$(document).ready(function(){
+	members.on("change", function(e){
+
+		e.preventDefault();
+		$(".temp").remove();
+		// permanent.append("<div class='temp'></div>");
+		for(var i=1; i<= members.val(); i++){
+			permanent.append(stringvalue(i));
+		}
+
+	});
+});
+	// lock.toggle();
