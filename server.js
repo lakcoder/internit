4919847af8f33b0e74d251a73148a856bb45f682
sagban_ <sagarbansal099@gmail.com>
2018-11-08 13:54:31 +0530

    dependencies

--- server.js
+++ server.js
@@ -2,42 +2,61 @@ const express = require('express');
 const fs = require('fs');
 const path = require('path');
 const hbs = require('hbs');
+var morgan  = require('morgan');
 var app = express();
 
+var passport   = require('passport');
+var session    = require('express-session');
+var bodyParser = require('body-parser');
+
+//For BodyParser
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// For Passport
+app.use(session({ secret: 'startupconclave',resave: true, saveUninitialized:true})); // session secret
+
+app.use(passport.initialize());
+
+app.use(passport.session()); // persistent login sessions
+
+//setting template engine
 app.set('view engine','hbs');
 app.use('/static', express.static(path.join(__dirname, 'public')));
 
-
+//NodeJS logger helps in debugging
+app.use(morgan('dev'));
 
 hbs.registerPartials(__dirname + '/views/includes');
 
 
-app.get('/',(req,res) =>{
+app.get('/',function(req,res){
   res.render('index.hbs') ;
 });
 
-app.get('/login',(req,res) =>{
+
+app.get('/login',function(req,res){
   res.render('login.hbs') ;
 });
 
-app.get('/register',(req,res) =>{
+app.get('/register',function(req,res){
   res.render('register.hbs') ;
 });
 
-app.get('/registerother',(req,res) =>{
+app.get('/registerother',function(req,res){
   res.render('registerother.hbs') ;
 });
 
 
-app.get('/ques',(req,res) =>{
+app.get('/ques',function(req,res){
   res.render('ques.hbs') ;
 });
 
-app.get('/dashboard',(req,res) =>{
+app.get('/dashboard',function(req,res){
   res.render('dashboard.hbs') ;
 });
 
 
-app.listen(3000,() =>{
+app.listen(3000,function(){
   console.log("server running on port 3000");
 });
